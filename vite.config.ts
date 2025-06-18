8609b1c337b05e1acd4f013002c7bd7f699a25fe
Ilovetechnologyman <teuglife875@gmail.com>
2025-06-18 19:07:11 +0200

    https local

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['lucide-react'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/lucide-react/, /node_modules/],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certificates/localhost.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certificates/localhost.crt')),
     },
-  },
+    port: 3000
+  }
 });
